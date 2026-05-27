e492fe89fbe070e8dc51aefe7b1e9337397eec68
timmyFe <spicemc@bk.ru>
2026-05-27 21:37:22 +0300

    add xml-json parser

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -6,10 +6,20 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,6 +28,10 @@ public class Main {
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         System.out.println(json);
+
+        List<Employee> xmlList = parseXML("data.xml");
+        String xmlJson = listToJson(xmlList);
+        writeString(xmlJson, "data2.json");
     }
 
     static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -40,5 +54,38 @@ public class Main {
     static String listToJson(List<Employee> list) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         return gson.toJson(list);
+    }
+
+    static List<Employee> parseXML(String fileName) {
+        List<Employee> employees = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(fileName));
+            Node root = doc.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    long id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+                    employees.add(new Employee(id, firstName, lastName, country, age));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
+    static void writeString(String content, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
